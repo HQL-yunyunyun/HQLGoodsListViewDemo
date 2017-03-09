3bc785b5f5aa235188382c19943b6a73037d024f
HQL-yunyunyun <he84965092@163.com>
2017-03-09 23:36:26 +0800

    一些更改

--- HQLGoodsListViewDemo/HQLGoodsListViewDemo/UITableView+EmptyView.h
+++ HQLGoodsListViewDemo/HQLGoodsListViewDemo/UITableView+EmptyView.h
@@ -8,8 +8,15 @@
 
 #import <UIKit/UIKit.h>
 
+@class HQLEmptyView;
+
 @interface UITableView (EmptyView)
 
+@property (assign, nonatomic) BOOL isShowEmptyView;
 
+@property (strong, nonatomic) UIImage *emptyImage;
+@property (copy, nonatomic) NSString *emptyTitle;
+@property (copy, nonatomic) void(^emptyTapBlock)();
+@property (strong, nonatomic) HQLEmptyView *emptyView;
 
 @end
